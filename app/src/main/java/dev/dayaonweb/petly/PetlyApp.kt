b458ee5272c57623e0d832eb747a0a4955dd8df9
dayamoy <dayamoy@uthaan.co.in>
2021-06-16 04:07:39 +0530

    UPDATE: Updated lib dependencies

--- app/src/main/java/dev/dayaonweb/petly/PetlyApp.kt
+++ app/src/main/java/dev/dayaonweb/petly/PetlyApp.kt
@@ -1,15 +1,53 @@
 package dev.dayaonweb.petly
 
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import dev.dayaonweb.petly.ui.theme.PetlyTheme
 
 
 @Composable
 fun PetlyApp() {
-    MaterialTheme {
-        Scaffold() {
 
+    val systemUiController = rememberSystemUiController()
+    SideEffect {
+        systemUiController.setSystemBarsColor(Color.White)
+    }
+
+    PetlyTheme {
+        Scaffold {
+            Map()
         }
+    }
+}
+
+@Composable
+fun Map() {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(
+            text = "Map screen",
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center,
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MapPreview() {
+    PetlyTheme {
+        Map()
     }
 }
